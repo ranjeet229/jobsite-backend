e7362b6a0312990f4ca57a8fac3436add620cb87
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-06 22:04:02 +0530

    final

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobsite-frontend-joa2.vercel.app',
     credentials: true
 }
 app.use(cors(corsOptions));
